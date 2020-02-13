235770cf05d7e4d871bb28a1aad530aca796a5bf
Gabe Shapiro <gabeshap@outlook.com>
2020-02-12 21:47:57 -0800

    first commit

---
+++ DevBootcampWebJobSDKSample/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace DevBootcampWebJobSDKSample
+{
+    public class Functions
+    {
+        public static void ProcessQueueMessage([QueueTrigger("myfakequeue")] string message, ILogger logger)
+        {
+            logger.LogInformation(message);
+        }
+    }
+}
---
+++ DevBootcampWebJobSDKSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace DevBootcampWebJobSDKSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
