7a256fd81c8bab24144c85309520292efa040272
Gabe Shapiro <gabeshap@outlook.com>
2020-04-07 07:51:13 -0700

    updated csproj

--- DevBootcampWebJobSDKSample/Functions.cs
+++ DevBootcampWebJobSDKSample/Functions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace DevBootcampWebJobSDKSample
 {
@@ -10,7 +11,17 @@ namespace DevBootcampWebJobSDKSample
     {
         public static void ProcessQueueMessage([QueueTrigger("myfakequeue")] string message, ILogger logger)
         {
+            var shutdownWatcher = new WebJobsShutdownWatcher();
+            shutdownWatcher.Token.Register(() => ShutdownRequested(logger));
+            Thread.Sleep(30000);
             logger.LogInformation(message);
+        }
+        private static void ShutdownRequested(ILogger logger)
+        {
+            logger.LogInformation("shutdown method entered");
+            //do some work before the shutdown
+            //Thread.Sleep(30000);
+            logger.LogInformation("shutdown method done");
         }
     }
 }
--- DevBootcampWebJobSDKSample/Program.cs
+++ DevBootcampWebJobSDKSample/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-
 namespace DevBootcampWebJobSDKSample
 {
     class Program
@@ -16,9 +15,18 @@ namespace DevBootcampWebJobSDKSample
                 b.AddAzureStorage();
             });
             builder.ConfigureLogging((context, b) =>
-            {
-                b.AddConsole();
-            });
+                {
+                    b.SetMinimumLevel(LogLevel.Information);
+                    b.AddConsole();
+
+                    // If this key exists in any config, use it to enable App Insights
+                    string instrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                    if (!string.IsNullOrEmpty(instrumentationKey))
+                    {
+                        b.AddApplicationInsightsWebJobs(o => o.InstrumentationKey = instrumentationKey);
+                    }
+                })
+                .UseConsoleLifetime();
             var host = builder.Build();
             using (host)
             {
